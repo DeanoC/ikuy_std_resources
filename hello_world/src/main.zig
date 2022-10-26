0e03eb951cfcc6170494e7654599067f691c8f04
Deano <me@deanoc.com>
2022-10-26 03:32:44 -0400

    tidy

--- hello_world/src/main.zig
+++ hello_world/src/main.zig
@@ -8,11 +8,13 @@ pub fn main() !void {
         .audio = true,
     });
     defer SDL.quit();
-    
+
     var window = try SDL.createWindow(
         "SDL2 Wrapper Demo",
-        .{ .centered = {} }, .{ .centered = {} },
-        640, 480,
+        .{ .centered = {} },
+        .{ .centered = {} },
+        640,
+        480,
         .{ .vis = .shown },
     );
     defer window.destroy();
@@ -33,24 +35,4 @@ pub fn main() !void {
 
         renderer.present();
     }
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
