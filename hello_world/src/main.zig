60203b8c2dfbacbfe129402eb58904d8a8d21c23
Deano <me@deanoc.com>
2022-10-28 14:58:49 -0400

    tidy

--- hello_world/src/main.zig
+++ hello_world/src/main.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const sdl = @import("sdl2");
-const VFile = @import("vfile.zig").VFile;
-const VFileMemory = @import("vfile_memory.zig").VFileMemory;
 
 pub fn main() !void {
     try sdl.init(.{
@@ -10,12 +8,6 @@ pub fn main() !void {
         .audio = true,
     });
     defer sdl.quit();
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var vfilemem: VFileMemory = try VFileMemory.initFromSize(allocator, 100, false);
-    var v: *VFile = &(vfilemem).vfile;
-    //    v.write()
-    defer v.close();
 
     var window = try sdl.createWindow(
         "SDL2 Wrapper Demo",
